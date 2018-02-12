fa768ec3bff30cbb1ee9e3b826666247341ba8bb
JiFish <joe@jifish.co.uk>
2018-02-12 20:18:24 +0000

    Move command processing in to own func, configurable max executions

--- warlockbot.php
+++ warlockbot.php
@@ -1,5 +1,8 @@
 <?php
 
+// Other configuration settings. You can override these in config.php if you wish
+define("MAX_EXECUTIONS",30);
+
 require('config.php');
 
 // Check the incoming data for the secret slack token
@@ -15,10 +18,231 @@ $commandlist = explode(";",$_POST['text']);
 $executions = 0;
 while (sizeof($commandlist) > 0)
 {
-    // Get the next command
-    $thiscommand = array_shift($commandlist);
+    // Process the next command in the list
+    processcommand(array_shift($commandlist));
     
-    $cmd = preg_split('/\s+/', trim($thiscommand));
+    // If stamina ever drops to less than 1, the player if dead
+    // Stop processing any queued commands and tell the player they are dead
+    if ($player['stam'] < 1) {
+        sendqmsg("_*You are dead.*_ :skull:",":skull:");
+        break;
+    }
+    
+    // Stop processing the queue after MAX_EXECUTIONS
+    if ($executions++ > MAX_EXECUTIONS) {
+        break;
+    }
+}
+
+save($player);
+
+
+
+
+function roll_character() {
+    $p = array('skill' => 6+rand(1,6), 'stam' => rand(1,6)+rand(1,6)+12, 'luck' => rand(1,6)+6, 'prov' => 10, 'gold' => rand(1,6)-1,
+                'weapon' => 0,
+                   'lastpage' => 1,
+                   'stuff' => array('Sword','Leather Armor','Lantern'));
+
+    // random potion
+    switch(rand(1,3)) {
+        case 1:
+            $p['stuff'][] = 'Potion of Skill';
+            break;
+        case 2:
+            $p['stuff'][] = 'Potion of Strength';
+            break;
+        case 3:
+            $p['stuff'][] = 'Potion of Luck';
+            $p['luck']++;
+            break;
+    }
+
+    // Set maximums
+    $p['max']['skill']  = $p['skill'];
+    $p['max']['stam']   = $p['stam'];
+    $p['max']['luck']   = $p['luck'];
+    $p['max']['prov']   = 99999;
+    $p['max']['gold']   = 99999;
+    $p['max']['weapon'] = 99999;
+
+    // Fluff
+    require('uinames.class.php');
+    $uinames = new uiNames();
+    $uin =$uinames->country('England')->fetch('Array');
+    $p['name'] = $uin['name'];
+    $p['icon'] = ($uin['gender']=='male'?':man:':':woman:');
+
+    $male = array(':boy:',':man:',':person_with_blond_hair:',':older_man:');
+    $female = array(':girl:',':woman:',':princess:',':older_woman:');
+    $skintone = array(':skin-tone-2:',':skin-tone-3:',':skin-tone-4:',':skin-tone-5:');
+    if ($uin['gender']=='male') {
+        $p['icon'] = $male[array_rand($male)].$skintone[array_rand($skintone)];
+    } else {
+        $p['icon'] = $female[array_rand($female)].$skintone[array_rand($skintone)];
+    }
+
+    return $p;
+}
+
+function load()
+{
+    $save = file_get_contents('save.txt');
+    if (!$save) {
+        $p = roll_character();
+    }
+    else {
+        $p = unserialize($save);
+    }
+
+    return $p;
+}
+
+function save($p)
+{
+    file_put_contents("save.txt",serialize($p));
+}
+
+function diceemoji($r)
+{
+    if ($r < 1 || $r > 6)
+        return "BADDICE";
+
+    return mb_convert_encoding('&#x'.(2679+$r).';', 'UTF-8', 'HTML-ENTITIES');
+}
+
+function addcommand($cmd)
+{
+    global $commandlist;
+    return array_unshift($commandlist,$cmd);
+}
+
+function send_charsheet($text = "")
+{
+    global $player;
+
+    $attachments = array([
+            'color'    => '#ff6600',
+            'fields'   => array(
+            [
+                'title' => 'Skill',
+                'value' => $player['skill']." / ".$player['max']['skill'],
+                'short' => true
+            ],
+            [
+                'title' => 'Stamina (stam)',
+                'value' => $player['stam']." / ".$player['max']['stam'],
+                'short' => true
+            ],
+            [
+                'title' => 'Luck',
+                'value' => $player['luck']." / ".$player['max']['luck'],
+                'short' => true
+            ],
+            [
+                'title' => 'Weapon Bonus (weapon)',
+                'value' => "+".$player['weapon'],
+                'short' => true
+            ],
+            [
+                'title' => 'Gold',
+                'value' => $player['gold'],
+                'short' => true
+            ],
+            [
+                'title' => 'Provisons (prov)',
+                'value' => $player['prov'],
+                'short' => true
+            ])
+    ]);
+    
+    if ($player['stam'] < 1) {
+        $icon = ":skull:";
+    } else {
+        $icon = $player['icon'];
+    }
+    
+    sendmsg("\n*".$player['name']."*",$attachments,$player['icon']);
+}
+
+function send_stuff()
+{
+    global $player;
+    $s = $player['stuff'];
+    if (sizeof($s) == 0) {
+        $s[] = "(Nothing!)";
+    } else {
+        natcasesort($s);
+        $s = array_map("ucfirst",$s);
+    }
+    
+    $attachments = array([
+            'color'    => '#0066ff',
+            'fields'   => array(
+            [
+                'title' => 'Inventory',
+                'value' => implode("\n",array_slice($s, 0, floor(sizeof($s) / 2))),
+                'short' => true
+            ],
+            [
+                'title' => "",
+                'value' => "\n".implode("\n",array_slice($s, floor(sizeof($s) / 2))),
+                'short' => true
+            ])
+    ]);
+    
+    if ($player['stam'] < 1) {
+        $icon = ":skull:";
+    } else {
+        $icon = $player['icon'];
+    }
+    
+    sendmsg("",$attachments,$icon);
+}
+
+function senddirmsg($message, $user = false)
+{
+    if (!$user) {
+        $user = $_POST['user_id'];
+    }
+    return sendmsg($message, true, ':green_book:', '@'.$user);
+}
+
+function sendqmsg($message, $icon = ':green_book:')
+{
+    return sendmsg($message, true, $icon);
+}
+
+function sendmsg($message, $attachments = array(), $icon = ':green_book:', $chan = false)
+{
+    global $player;
+    
+    $data = array(
+        'text'        => $message,
+        'icon_emoji'  => $icon,
+        'attachments' => $attachments
+    );
+    if ($chan) {
+        $data['channel'] = $chan;
+    }
+    $data_string = json_encode($data);
+    $ch = curl_init(SLACK_HOOK);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string))
+        );
+    //Execute CURL
+    $result = curl_exec($ch);
+    return $result;        
+}
+
+function processcommand($command)
+{
+    $cmd = preg_split('/\s+/', trim($command));
     
     $cmd[0] = substr(strtolower($cmd[0]),1);
     if (sizeof($cmd) > 1) {
@@ -362,221 +586,4 @@ while (sizeof($commandlist) > 0)
         }
         sendqmsg($out,":crossed_swords:");
     }
-    
-    //DEATH CHECK
-    //also break on death
-    if ($player['stam'] < 1) {
-        sendqmsg("_*You are dead.*_ :skull:",":skull:");
-        break;
-    }
-    //break on too many executions
-    if ($executions++ > 30) {
-        break;
-    }
-}
-
-save($player);
-
-
-
-
-function roll_character() {
-    $p = array('skill' => 6+rand(1,6), 'stam' => rand(1,6)+rand(1,6)+12, 'luck' => rand(1,6)+6, 'prov' => 10, 'gold' => rand(1,6)-1,
-                'weapon' => 0,
-                   'lastpage' => 1,
-                   'stuff' => array('Sword','Leather Armor','Lantern'));
-    
-    // random potion
-    switch(rand(1,3)) {
-        case 1:
-            $p['stuff'][] = 'Potion of Skill';
-            break;
-        case 2:
-            $p['stuff'][] = 'Potion of Strength';
-            break;
-        case 3:
-            $p['stuff'][] = 'Potion of Luck';
-            $p['luck']++;
-            break;
-    }
-    
-    // Set maximums
-    $p['max']['skill']  = $p['skill'];
-    $p['max']['stam']   = $p['stam'];
-    $p['max']['luck']   = $p['luck'];
-    $p['max']['prov']   = 99999;
-    $p['max']['gold']   = 99999;
-    $p['max']['weapon'] = 99999;
-    
-    // Fluff
-    require('uinames.class.php');
-    $uinames = new uiNames();
-    $uin =$uinames->country('England')->fetch('Array');
-    $p['name'] = $uin['name'];
-    $p['icon'] = ($uin['gender']=='male'?':man:':':woman:');
-    
-    $male = array(':boy:',':man:',':person_with_blond_hair:',':older_man:');
-    $female = array(':girl:',':woman:',':princess:',':older_woman:');
-    $skintone = array(':skin-tone-2:',':skin-tone-3:',':skin-tone-4:',':skin-tone-5:');
-    if ($uin['gender']=='male') {
-        $p['icon'] = $male[array_rand($male)].$skintone[array_rand($skintone)];
-    } else {
-        $p['icon'] = $female[array_rand($female)].$skintone[array_rand($skintone)];
-    }
-    
-    return $p;
-}
-
-function load()
-{
-    $save = file_get_contents('save.txt');
-    if (!$save) {
-        $p = roll_character();
-    }
-    else {
-        $p = unserialize($save);
-    }
-    
-    return $p;
-}
-
-function save($p)
-{
-    file_put_contents("save.txt",serialize($p));
-}
-
-function diceemoji($r)
-{
-    if ($r < 1 || $r > 6)
-        return "BADDICE";
-        
-    return mb_convert_encoding('&#x'.(2679+$r).';', 'UTF-8', 'HTML-ENTITIES');
-}
-
-function addcommand($cmd)
-{
-    global $commandlist;
-    return array_unshift($commandlist,$cmd);
-}
-
-function send_charsheet($text = "")
-{
-    global $player;
-    
-    $attachments = array([
-            'color'    => '#ff6600',
-            'fields'   => array(
-            [
-                'title' => 'Skill',
-                'value' => $player['skill']." / ".$player['max']['skill'],
-                'short' => true
-            ],
-            [
-                'title' => 'Stamina (stam)',
-                'value' => $player['stam']." / ".$player['max']['stam'],
-                'short' => true
-            ],
-            [
-                'title' => 'Luck',
-                'value' => $player['luck']." / ".$player['max']['luck'],
-                'short' => true
-            ],
-            [
-                'title' => 'Weapon Bonus (weapon)',
-                'value' => "+".$player['weapon'],
-                'short' => true
-            ],
-            [
-                'title' => 'Gold',
-                'value' => $player['gold'],
-                'short' => true
-            ],
-            [
-                'title' => 'Provisons (prov)',
-                'value' => $player['prov'],
-                'short' => true
-            ])
-    ]);
-    
-    if ($player['stam'] < 1) {
-        $icon = ":skull:";
-    } else {
-        $icon = $player['icon'];
-    }
-    
-    sendmsg("\n*".$player['name']."*",$attachments,$player['icon']);
-}
-
-function send_stuff()
-{
-    global $player;
-    $s = $player['stuff'];
-    if (sizeof($s) == 0) {
-        $s[] = "(Nothing!)";
-    } else {
-        natcasesort($s);
-        $s = array_map("ucfirst",$s);
-    }
-    
-    $attachments = array([
-            'color'    => '#0066ff',
-            'fields'   => array(
-            [
-                'title' => 'Inventory',
-                'value' => implode("\n",array_slice($s, 0, floor(sizeof($s) / 2))),
-                'short' => true
-            ],
-            [
-                'title' => "",
-                'value' => "\n".implode("\n",array_slice($s, floor(sizeof($s) / 2))),
-                'short' => true
-            ])
-    ]);
-    
-    if ($player['stam'] < 1) {
-        $icon = ":skull:";
-    } else {
-        $icon = $player['icon'];
-    }
-    
-    sendmsg("",$attachments,$icon);
-}
-
-function senddirmsg($message, $user = false)
-{
-    if (!$user) {
-        $user = $_POST['user_id'];
-    }
-    return sendmsg($message, true, ':green_book:', '@'.$user);
-}
-
-function sendqmsg($message, $icon = ':green_book:')
-{
-    return sendmsg($message, true, $icon);
-}
-
-function sendmsg($message, $attachments = array(), $icon = ':green_book:', $chan = false)
-{
-    global $player;
-    
-    $data = array(
-        'text'        => $message,
-        'icon_emoji'  => $icon,
-        'attachments' => $attachments
-    );
-    if ($chan) {
-        $data['channel'] = $chan;
-    }
-    $data_string = json_encode($data);
-    $ch = curl_init(SLACK_HOOK);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string))
-        );
-    //Execute CURL
-    $result = curl_exec($ch);
-    return $result;        
 }
