363a2f3a0e113c9d23a405def7fd1b74a3136679
JiFish <joe@jifish.co.uk>
2018-12-03 00:57:42 +0000

    Add book convert script

---
+++ bookconvert.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!isset($argv[1])) {
+    die("Usage: bookconvert.php [book.txt]");
+}
+
+if (!file_exists($argv[1])) {
+    die ("File not found.");
+}
+
+$book = array();
+$input = file($argv[1]);
+$page = "";
+$expected_num = 1;
+
+foreach($input as $line)
+{
+    $line = trim($line);
+    if ($line == "") { continue; }
+    if (is_numeric($line)) {
+        if ($line != $expected_num) {
+            die("Unexpected page number. Expected $expected_num, got $line");
+        }
+        $book[] = trim($page);
+        $page = "";
+        $expected_num++;
+        continue;
+    }
+    $page .= $line . "\n";
+}
+
+echo ("<?php\n\n");
+echo var_export($book,1).";";
