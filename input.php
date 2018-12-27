4cd4d125bab169c7a66130cefd00f57d8966d108
JiFish <joe@jifish.co.uk>
2018-12-27 04:14:25 +0000

    Adds input.php for testing

---
+++ input.php
@@ -0,0 +1,22 @@
+<?php
+
+// This file is useful for testing from the command line e.g.
+// php input.txt !echo Hello world!
+
+// You could also put it online by editing .htaccess, allowing
+// you to create alternate ways of inputting commands, e.g. a webform
+
+if (isset($argv[1])) {
+    $_POST['text'] = implode(" ",array_slice($argv,1));
+} else if (isset($_REQUEST['c'])) {
+    $_POST['text'] = $_REQUEST['c'];
+} else {
+    die("No command!");
+}
+
+require_once('config.php');
+
+$_POST['trigger_word'] = '!';
+$_POST['token'] = SLACK_TOKEN;
+
+require('storyteller.php');
