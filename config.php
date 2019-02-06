e8e2fae5f696a97997d1cf53a6ae14bdb4367700
JiFish <joe@jifish.co.uk>
2019-02-06 19:57:22 +0000

    Cleanup config

--- config.php
+++ config.php
@@ -3,6 +3,12 @@
 // URL of incoming webhook
 define("SLACK_HOOK","https://hooks.slack.com/services/YOUR/WEBHOOK/HERE");
 
+// Token for outgoing webhook
+define("SLACK_TOKEN","YOURTOKENHERE");
+
+// Book type code. This determines which rules are used. See in extras to find your code
+define("BOOK_TYPE",'wofm');
+
 // Types of dice rolling rules when creating characters
 // normal : Roll as normal
 // min3   : Rolls of 1 or 2 are replaced with 3
@@ -12,9 +18,8 @@ define("SLACK_HOOK","https://hooks.slack.com/services/YOUR/WEBHOOK/HERE");
 // all6   : Use 6 for all dice, instead of rolling
 define("CHARACTER_ROLLS",'normal');
 
-// Token for outgoing webhook
-define("SLACK_TOKEN","YOURTOKENHERE");
-define("BOOK_TYPE",'wofm');
-
-define("MAX_EXECUTIONS",30);
+// Discord mode formats output to work with Discord's *Slack Compatible* incoming webhooks
 define("DISCORD_MODE",false);
+
+// Maximum command executions in one go. You probably won't need to change this.
+define("MAX_EXECUTIONS",30);
